6d0c5b7205e15abb8a915096f4ceaf0705e624fc
Kunal Chitroda <kunalchitroda3351@gmail.com>
2025-10-08 21:01:31 +0530

    updated model_evaluation.py

--- src/model_evaluation.py
+++ src/model_evaluation.py
@@ -1,24 +1,29 @@
-# src/model_evaluation.py
-
 import pandas as pd
 import joblib
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 import os
+import sys
+import yaml
 import json
-import mlflow  # Import mlflow
+import mlflow
 
 # --- Configuration ---
 PROCESSED_DATA_FOLDER = 'data/processed'
 MODEL_FOLDER = 'models'
 MODEL_NAME = 'random_forest_model.pkl'
 METRICS_FILE = 'metrics.json'
-PARAMS_FILE = 'best_params.json' # Add path to params file
 
-def evaluate_model():
+def evaluate_model(params_path):
     """
-    Evaluates the model and logs parameters and metrics to MLflow.
+    Evaluates the model and logs fixed parameters and metrics to MLflow.
     """
     print("Starting model evaluation and MLflow logging...")
+
+    # Load parameters from the main YAML file
+    with open(params_path) as f:
+        params = yaml.safe_load(f)
+
+    train_params = params['train']
 
     # Start a new MLflow run for evaluation
     with mlflow.start_run():
@@ -26,14 +31,10 @@ def evaluate_model():
         model = joblib.load(os.path.join(MODEL_FOLDER, MODEL_NAME))
         X_test = pd.read_csv(os.path.join(PROCESSED_DATA_FOLDER, 'X_test.csv'))
         y_test = pd.read_csv(os.path.join(PROCESSED_DATA_FOLDER, 'y_test.csv')).values.ravel()
-
-        # Load the parameters that were used to train this model
-        with open(PARAMS_FILE) as f:
-            params = json.load(f)
         
         # --- MLFLOW: Log the parameters ---
-        print(f"Logging parameters: {params}")
-        mlflow.log_params(params)
+        print(f"Logging parameters: {train_params}")
+        mlflow.log_params(train_params)
 
         # Make predictions
         y_pred = model.predict(X_test)
@@ -55,4 +56,4 @@ def evaluate_model():
         print(f"Metrics saved to '{METRICS_FILE}' for DVC.")
 
 if __name__ == '__main__':
-    evaluate_model()
+    evaluate_model(params_path='params.yaml')
